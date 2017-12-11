e70cad6abdbe9ab7d2df7a318454c490bd3b9b63
JProtyp <33751893+JProtyp@users.noreply.github.com>
2017-12-11 20:39:53 +0100

    Create Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,11 @@
+import serial
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+print("Starting!")
+
+while True:
+      comando = raw_input('Introduce un comando: ') #Input
+      arduino.write(comando) #Mandar un comando hacia Arduino
+
+arduino.close() #Finalizamos la comunicacion
