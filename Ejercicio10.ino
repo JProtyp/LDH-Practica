278c489537a19844633320c014cd9059f1630c5a
JProtyp <33751893+JProtyp@users.noreply.github.com>
2017-12-11 21:29:19 +0100

    Update Ejercicio10.ino

--- Ejercicio10.ino
+++ Ejercicio10.ino
@@ -1,32 +1,3 @@
-/**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * DESCRIPTION
- *
- * Simple binary switch example 
- * Connect button or door/window reed switch between 
- * digitial I/O pin 3 (BUTTON_PIN below) and GND.
- * http://www.mysensors.org/build/binary
- */
-
-
 // Enable debug prints to serial monitor
 #define MY_DEBUG 
 
@@ -44,31 +15,17 @@
 #include <Bounce2.h>
 
 #define CHILD_ID 3
-#define CHILD_ID2 4
 #define BUTTON_PIN  3  // Arduino Digital I/O pin for button/reed switch
-#define BUTTON_PIN2  4  // Arduino Digital I/O pin for button/reed switch
-#define RELAY_1  5  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 2 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
 
 Bounce debouncer = Bounce(); 
-Bounce debouncer2 = Bounce(); 
 int oldValue=-1;
-int oldValue2=-1;
 
 
 // Change to V_LIGHT if you use S_LIGHT in presentation below
 MyMessage msg(CHILD_ID,V_TRIPPED);
-MyMessage msg2(CHILD_ID2,V_TRIPPED);
 void before()
 {
-  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
-    // Then set relay pins in output mode
-    pinMode(pin, OUTPUT);
-    // Set relay to last known state (using eeprom storage)
-    digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
-  }
+
 }
 void setup()  
 {  
@@ -76,15 +33,10 @@ void setup()
   pinMode(BUTTON_PIN,INPUT);
   // Activate internal pull-up
   digitalWrite(BUTTON_PIN,HIGH);
-  pinMode(BUTTON_PIN2,INPUT);
-  // Activate internal pull-up
-  digitalWrite(BUTTON_PIN2,HIGH);
 
   // After setting up the button, setup debouncer
   debouncer.attach(BUTTON_PIN);
   debouncer.interval(5);
-  debouncer2.attach(BUTTON_PIN2);
-  debouncer2.interval(5);
 
 }
 
@@ -93,13 +45,8 @@ void presentation() {
   // You can use S_DOOR, S_MOTION or S_LIGHT here depending on your usage. 
   // If S_LIGHT is used, remember to update variable type you send in. See "msg" above.
   present(CHILD_ID, S_DOOR);  
-  present(CHILD_ID2, S_DOOR);  
-  sendSketchInfo("Relay", "1.0");
-
-  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
-    // Register all sensors to gw (they will be created as child devices)
-    present(sensor, S_BINARY);
-}
+  sendSketchInfo("Door", "1.0");
+  present(BUTTON_PIN, S_BINARY);
 }
 
 
@@ -107,34 +54,12 @@ void presentation() {
 void loop() 
 {
   debouncer.update();
-  debouncer2.update();
   // Get the update value
   int value = debouncer.read();
-  int value2 = debouncer2.read();
   
   if (value != oldValue) {
      // Send in the new value
      send(msg.set(value==HIGH ? 1 : 0));
      oldValue = value;
-  }
-   if (value2 != oldValue2) {
-     // Send in the new value
-     send(msg2.set(value2==HIGH ? 1 : 0));
-     oldValue2 = value2;
-  }
-}
-void receive(const MyMessage &message)
-{
-  // We only expect one type of message from controller. But we better check anyway.
-  if (message.type==V_STATUS) {
-    // Change relay state
-    digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
-    // Store state in eeprom
-    saveState(message.sensor, message.getBool());
-    // Write some debug info
-    Serial.print("Incoming change for sensor:");
-    Serial.print(message.sensor);
-    Serial.print(", New status: ");
-    Serial.println(message.getBool());
   }
 }
