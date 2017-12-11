1590d504ace24a79f35e343de1315b37c0e71758
JProtyp <33751893+JProtyp@users.noreply.github.com>
2017-12-11 20:38:10 +0100

    Create Ejercicio5.ino

---
+++ Ejercicio5.ino
@@ -0,0 +1,23 @@
+     
+    #include <LiquidCrystal.h>  
+
+    LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+      int i =0;
+    void setup() {  
+      Serial.begin(9600);
+      
+       //impostiamo il numero di colonne ed il numero di righe di lcd  
+      lcd.begin(16, 2);  
+      // Visualizzo il messaggio sul display  
+      lcd.print("Salve mondo!");  
+    }  
+      
+    void loop() {  
+      
+      while (Serial.available()){
+        char inChar = (char)Serial.read();
+        lcd.setCursor(i,1);
+        lcd.print(inChar);
+        i=i+1;
+      } 
+    }  
